f29893147b25674b478dd62812a528967b7126f2
MahammadShameer211 <Mahammad.Shameer@bosch.com>
2025-07-15 17:03:09 +0530

    Test

--- src/main/java/vitals/VitalsChecker.java
+++ src/main/java/vitals/VitalsChecker.java
@@ -11,7 +11,6 @@ public abstract class VitalsChecker {
         Thread.sleep(1000);
         System.out.print("\r *");
         Thread.sleep(1000);
-        
       }
       return false;
     } else if (pulseRate < 60 || pulseRate > 100) {
